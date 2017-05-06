b5731051de9c0fd8a6c0b52f5e6e6676a7d88836
4iar <me@4iar.com>
2017-05-06 21:11:21 +0100

    Add basic event page functionality

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -8,20 +8,52 @@ chrome.browserAction.setBadgeText({ text: '\'Allo' });
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
 
-function start(url) {
+function start(url, alarmInfo={periodInMinutes: 0.2}) {
+  chrome.alarms.create("pizza", alarmInfo);
 }
 
 function stop() {
+  chrome.storage.sync.set({ url: '' });
+  chrome.alarms.clearAll();
 }
 
 function handleStatus(status) {
+  // Store the recent status to make it available for use by
+  // the popup frontend
   chrome.storage.sync.set(status);
 }
 
 function getStatus(url) {
+  if (!url) {
+    return;
+  }
+
+  const idToMessage = {
+    1: '?',
+    2: '?',
+    6: 'ordered',
+    5: 'baking',
+    8: 'quality check',
+    9: 'delivered',
+    3: 'delivered'
+  }
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, false);
+  xhr.send();
+
+  var result = JSON.parse(xhr.responseText);
+  var status = {
+    id: result.statusId,
+    message: idToMessage[result.statusId]
+  };
+
+  chrome.storage.sync.set(status);
 };
 
 module.exports = {
   getStatus: getStatus,
-  handleStatus: handleStatus
+  handleStatus: handleStatus,
+  stop: stop,
+  start: start,
 };
--- app/scripts.babel/background.test.js
+++ app/scripts.babel/background.test.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const assert = require('chai').assert;
+
 global.chrome = require('sinon-chrome');
 
 const {
@@ -17,19 +18,47 @@ describe('background', function () {
 
  describe('stop', function () {
     it('clears remaining alarms', function () {
+      assert.ok(
+        chrome.alarms.clearAll.notCalled,
+        'alarms should not be cleared yet'
+      );
+      stop();
+      assert.ok(
+        chrome.alarms.clearAll.calledOnce,
+        'alarms should be cleared'
+      );
     });
 
     it('clears the tracking url from localstorage', function () {
+      assert.ok(
+        chrome.storage.sync.set.withArgs({ url: '' }).notCalled,
+        'storage should not be cleared yet'
+      );
+      stop();
+      assert.ok(
+        chrome.storage.sync.set.withArgs({ url: '' }).calledOnce,
+        'storage should be cleared'
+      );
     });
   });
 
   describe('start', function () {
-    it('starts the alarm', function () {
+    it('starts the alarm timer with default timing information', function () {
+      assert.ok(chrome.alarms.create.notCalled, 'alarm should not be created yet');
+      start();
+      assert.ok(chrome.alarms.create.calledOnce, 'alarm should be created');
+    });
+
+    xit('starts the timer with specified timing information', function () {
     });
   });
 
   describe('getStatus', function () {
-    it('gets the status of a delivery order given the url', function () {
+    it('returns undefined if there is no url', function () {
+      expect(getStatus()).to.be.undefined;
+    });
+
+    xit('gets the status of a delivery order given the url', function () {
     });
   });
 
@@ -45,7 +74,10 @@ describe('background', function () {
       assert.ok(chrome.storage.sync.set.withArgs(theStatus).calledOnce);
     });
 
-    it('sets the badge text', function () {
+    xit('sets the badge text', function () {
+    });
+
+    xit('sends a notification if the status has changed', function () {
     });
   });
 });
