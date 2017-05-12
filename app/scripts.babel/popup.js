07b0211ab0a80ca1c15025ea0847fd7262ca5869
4iar <me@4iar.com>
2017-05-12 18:22:05 +0100

    Implement popup status update

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,9 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  for (key in changes) {
+    if (key == 'message') {
+      document.getElementById('status').innerHTML = changes[key].newValue
+    }
+  }
+});
